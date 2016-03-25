3435eb74055c730f3c379682b95a6cde5324641b
Beata Bubula <beatabubula@interia.pl>
2016-03-25 10:49:53 +0100

    geting file size

---
+++ filesSize.py
@@ -0,0 +1,20 @@
+import os
+from PIL import Image
+
+#dir=location where images are stored. In our case it's "photo"
+#when the dir isn't in our fileSize.py location we have to give e.g. "C:\\Users\\beata_000\\Desktop\\BO\\photo"
+
+#return list of image size: (width,height)
+
+def get_image_size(dir):
+    x =[]
+    y=[]
+    for root, dirs, files in os.walk(dir):
+        for name in files:
+            print name
+            im = Image.open(dir+"\\"+name)
+            print im.size
+            x.append(im.size[0])
+            y.append(im.size[1])
+        list = zip(x,y)
+        return list
---
+++ main.py
@@ -0,0 +1,9 @@
+
+
+from filesSize import get_image_size
+
+
+if __name__ == '__main__':
+    dir = "photo"
+
+    print get_image_size(dir)
